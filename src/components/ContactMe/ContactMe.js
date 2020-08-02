7a686f36efb6c264674e0bf623498e1fffafb5b2
mahmoud 32salamn <mahmoud32salamn@gmail.com>
2020-08-02 19:28:45 +0200

    responsive  contactme  page

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -6,29 +6,35 @@ const ContactMe = () => {
     <section className="Container">
       <div>
         <i className="fa fa-github fa-2x" aria-hidden="true"></i>
-        <a className="github" href="https://github.com/mahmoud-farag">
-          Github Account
-        </a>
+        <p>
+          <a className="github" href="https://github.com/mahmoud-farag">
+            Github Account
+          </a>
+        </p>
       </div>
       <hr></hr>
       <div>
         <i className="fa fa-linkedin fa-2x" aria-hidden="true"></i>
-        <a
-          className="linkedin"
-          href="https://www.linkedin.com/in/mahmoud-farag-4346a4162/"
-        >
-          LinkedIn Account
-        </a>
+        <p>
+          <a
+            className="linkedin"
+            href="https://www.linkedin.com/in/mahmoud-farag-4346a4162/"
+          >
+            LinkedIn Account
+          </a>
+        </p>
       </div>
       <hr></hr>
       <div>
         <i className="fa fa-facebook fa-2x" aria-hidden="true"></i>
-        <a
-          className="facebook"
-          href="https://www.facebook.com/mahmoud.farag.18294053"
-        >
-          Facebooke Account
-        </a>
+        <p>
+          <a
+            className="facebook"
+            href="https://www.facebook.com/mahmoud.farag.18294053"
+          >
+            Facebooke Account
+          </a>
+        </p>
       </div>
 
       <hr></hr>
